7ba7b97bc12f6df5f6b83e95f2b0588831f02652
I-am-Chittesh <iamchitteshd@gmail.com>
2026-08-19 10:50:20 +0530

    sensors

---
+++ sensors.py
@@ -0,0 +1,12 @@
+import random
+from grid_world import Cell
+
+def read_gpr(cell: Cell) -> float:
+    if cell.is_threat:
+        return round(random.uniform(0.70, 0.99), 2)
+    return round(random.uniform(0.05, 0.35), 2)
+
+def read_cv(cell: Cell) -> float:
+    if cell.is_threat:
+        return round(random.uniform(0.60, 0.95), 2)
+    return round(random.uniform(0.00, 0.40), 2)
